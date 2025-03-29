cc503aa511a0103ba939a4bcbc892bbddc5888f0
Dustin Clark <138034989+Dclark2434@users.noreply.github.com>
2025-03-28 22:16:49 -0500

    Enhance TextNode class with URL handling and improve equality and representation methods

--- src/textnode.py
+++ src/textnode.py
@@ -10,4 +10,15 @@ class TextType(Enum):
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
+        if url == "":
+            url = None
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return self == other
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
         
