0a5d71ca5249577c12d28d52bbfa881dad9b8274
Dustin Clark <138034989+Dclark2434@users.noreply.github.com>
2025-03-28 22:24:25 -0500

    Remove unused HTML and CSS files; update main.py to test TextNode functionality

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    tester = TextNode("This is a test of the emerg", "link", "NOAA.ORG")
+    print(f"{tester}")
+    
+
+
+main()
